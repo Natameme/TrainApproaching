17c0765e97a8ef518b0fde191b66667bcb6f82d9
Natameme <60371038+Natameme@users.noreply.github.com>
2020-11-05 22:14:50 -0500

    encapsulated first set of functions in main(), still need work on getdist()

--- TrainAppGeneralized.js
+++ TrainAppGeneralized.js
@@ -1,3 +1,12 @@
+//Variable Placeholders
+var Tstatus = []; /*stores output of loop*/
+var Tdist = [];
+var Tlength;
+var ucoor = [];
+var ulat;
+var ulon;
+
+
 //MBTA API Link
 const api_url = 'https://api-v3.mbta.com/vehicles?filter[route]=Green-B,Green-C,Green-D,Green-E,Orange,Blue,Red,Mattapan,CR-Worcester,CR-Newburyport,CR-Middleborough,CR-Greenbush,CR-Lowell,CR-Franklin,CR-Fitchburg,CR-Haverhill,CR-Providence';
 /*
@@ -5,15 +14,15 @@ route IDs
 Green-B,Green-C,Green-D,Green-E,Orange,Blue,Red,Mattapan,CR-Worcester,CR-Newburyport,CR-Middleborough,CR-Greenbush,CR-Lowell,CR-Franklin,CR-Fitchburg,CR-Haverhill,CR-Providence
 */
 //pulls data, and length of index (# of trains on the line)
+
+
+function main(){
+//getTrain Function
 async function getTrain(){
   const response = await fetch (api_url);
   const Tdata = await response.json();
   const L = Tdata.data.length;
   const J = L-1;
-
-
-
-var Tstatus = ['']; /*stores output of loop*/
 
 /*loop pulls train status, stop, and direction
 for each train, J is L-1 (# of trains on the line)
@@ -23,34 +32,51 @@ which dictates how many time the loop repeats*/
           var vid = Tdata.data[i].relationships.route.data.id;
           var lati =  Tdata.data[i].attributes.latitude;
           var loni =  Tdata.data[i].attributes.longitude;
-          var coor = {ID: vid , Lat: lati , Lon: loni}
+          var headi = Tdata.data[i].attributes.bearing;
+          var coor = {ID: vid , Lat: lati , Lon: loni, Heading: headi}
           Tstatus.push(coor);
           }
+          Tlength = await Tstatus.length;
 
-      //Logs status of trains into HTML element
-      console.log(Tstatus);
-
+          //products of getTrain
+          console.log(Tstatus);
+          console.log(Tlength);
 }
 
 getTrain();
+//GetUserLocation
+function getuloc(){
+if ('geolocation' in navigator) {
+  console.log('Geolocation available');
+  navigator.geolocation.getCurrentPosition(function(position) {
+    var ulat = position.coords.latitude;
+    var ulon = position.coords.longitude;
+    var ucoor = {lat: ulat, lon: ulon};
+    console.log(ucoor);
+  });
+} else {
+  console.log('geolocation not available');
+}
+}
 
-//Math Code lifted from user Neuron on Stack Overflow
-//https://stackoverflow.com/questions/18883601/function-to-calculate-distance-between-two-coordinates
+getuloc();
 
-    function getDistanceFromLatLonInKm(lat, lon, Ulat, Ulon) {
-      var R = 6371; // Radius of the earth in km
-      var dLat = deg2rad(lat2-lat1);  // deg2rad below
-      var dLon = deg2rad(lon2-lon1);
-      var a =
-        Math.sin(dLat/2) * Math.sin(dLat/2) +
-        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
-        Math.sin(dLon/2) * Math.sin(dLon/2)
-        ;
-      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-      var d = R * c; // Distance in km
-      return d;
+}
+
+main();
+//Calculate Distance Between User and Trains
+async function getDist() {
+
+      for(k=0; k<Tlength; k++){
+      d = Math.sqrt(Math.pow(ulat-Tstatus[k].lat, 2) + Math.cos(Tstatus[k].lat)* Math.pow(ulon-Tstatus[k].lon, 2));
+      Tdist.push(d);
     }
+    console.log(Tdist);
+}
 
-    function deg2rad(deg) {
-      return deg * (Math.PI/180)
-    }
+getDist();
+
+
+
+
+////////*THE LAND OF BROKEN CODE*///////
