db40d60dc0bcc6221124792ed2630146dd8d288d
Natameme <60371038+Natameme@users.noreply.github.com>
2020-11-06 14:04:37 -0500

    made trainnotif.js an external file

--- Trainnotif.js
+++ Trainnotif.js
@@ -1,32 +1,42 @@
 //MBTA API Link
 const api_url = 'https://api-v3.mbta.com/vehicles?filter[route]=CR-Worcester&include=stop';
-//Pulls train data from MBTA
-async function getTrain()
-	{
+//pulls data, and length of index (# of trains on the line)
+async function getTrain(){
 	const response = await fetch (api_url);
 	const Tdata = await response.json();
-//pulls vehicle number, next stop, and vehicle direction
-		for(i=0;i<=4;i++)
-		{
-		const stops = Tdata.included[i].id;
-		const dir =  Tdata.data[i].attributes.direction_id;
-		var inf = stops + ' ' + dir;
-//Prints Data to Console
-		console.log(inf);
-//Conditional Logic to see if there is a Train Approaching
-			switch(inf)
-			{
-				case "Boston Landing O":
-				console.log('Train Approaching Outbound');
-				
-				break;
-				case "Yawkey 1":
-				console.log('Train Approaching Inbound');
-				break;
-				default:
-				null
-			}
-		}
-	}
+	const L = Tdata.included.length;
+	const J = L-1;
 
-	setInterval(getTrain(),3000);
+
+var Tstatus = ['']; /*stores output of loop*/
+
+//loop pulls train status, stop, and direction for each train, J is L-1 (# of trains on the line) which dictates how many time the loop repeats
+		for(i=0;i<=J;i++){
+					const cur =  Tdata.data[i].attributes.current_status;
+					const stops = Tdata.included[i].id;
+					const dir =  Tdata.data[i].attributes.direction_id;
+					var inf = cur + ' ' + stops + ' ' + dir;
+					Tstatus.push(inf);
+					console.log(Tstatus)
+					}
+
+			//Logs status of trains into HTML element
+			document.getElementById("stat").innerHTML = Tstatus;
+
+			//Conditional Logic to see if there is a Train Approaching
+			if (Tstatus.includes('IN_TRANSIT_TO Yawkey 0') == true){
+			document.getElementById("notif").innerHTML = "Train Approaching Inbound";
+			document.body.style.backgroundColor = "red";
+			}
+			else if (Tstatus.includes('IN_TRANSIT_TO Boston Landing 1') == true){
+			document.getElementById("notif").innerHTML = "Train Approaching Outbound";
+			document.body.style.backgroundColor = "red";
+			}
+			else{
+			document.getElementById("notif").innerHTML = "No Trains Nearby";
+			document.body.style.backgroundColor = rgba(84,150,89,1 );
+			}
+
+}
+getTrain();
+setInterval(function(){getTrain();}, 5000);
