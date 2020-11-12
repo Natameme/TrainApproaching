7c74181128192162d7e3153c9a264ccee74c6434
Natameme <60371038+Natameme@users.noreply.github.com>
2020-11-12 14:04:46 -0500

    added haversine calculation and conditional logic

--- TrainAppGeneralized.js
+++ TrainAppGeneralized.js
@@ -13,68 +13,73 @@ const api_url = 'https://api-v3.mbta.com/vehicles?filter[route]=Green-B,Green-C,
 route IDs
 Green-B,Green-C,Green-D,Green-E,Orange,Blue,Red,Mattapan,CR-Worcester,CR-Newburyport,CR-Middleborough,CR-Greenbush,CR-Lowell,CR-Franklin,CR-Fitchburg,CR-Haverhill,CR-Providence
 */
-//pulls data, and length of index (# of trains on the line)
 
+//GetUserLocation
+function getuloc(callback){
+      if ('geolocation' in navigator) {
+        console.log('Geolocation available');
+        navigator.geolocation.getCurrentPosition(function(position) {
+        let ulat = position.coords.latitude;
+        let ulon = position.coords.longitude;
+        console.log(ulat);
+        console.log(ulon);
+        callback(ulat,ulon);
+        });
 
-function main(){
+      } else {
+        document.getElementById("notif").innerHTML = 'geolocation not available';
+          }
+
+  }
+//Haversine formula, calculates distance between 2 latlon points
+function haversine_distance(lat1,lon1,lat2,lon2) {
+      var R = 637107.10; // Radius of the Earth in meters
+      var rlat1 = lat1 * (Math.PI/180); // Convert degrees to radians
+        var rlat2 = lat2 * (Math.PI/180); // Convert degrees to radians
+        var difflat = rlat2-rlat1; // Radian difference (latitudes)
+        var difflon = (lon2-lon1) * (Math.PI/180); // Radian difference (longitudes)
+
+        var d = 2 * R * Math.asin(Math.sqrt(Math.sin(difflat/2)*Math.sin(difflat/2)+Math.cos(rlat1)*Math.cos(rlat2)*Math.sin(difflon/2)*Math.sin(difflon/2)));
+        return d;
+      }
+
 //getTrain Function
-async function getTrain(){
-  const response = await fetch (api_url);
+async function getTrain(ulat,ulon){
+  const response = await fetch(api_url);
   const Tdata = await response.json();
   const L = Tdata.data.length;
   const J = L-1;
-
 /*loop pulls train status, stop, and direction
 for each train, J is L-1 (# of trains on the line)
 which dictates how many time the loop repeats*/
+
     for(i=0;i<=J;i++){
       //vehicle ID call
           var vid = Tdata.data[i].relationships.route.data.id;
           var lati =  Tdata.data[i].attributes.latitude;
           var loni =  Tdata.data[i].attributes.longitude;
           var headi = Tdata.data[i].attributes.bearing;
-          var coor = {ID: vid , Lat: lati , Lon: loni, Heading: headi}
+          var distance = haversine_distance(lati,loni, ulat, ulon);
+          var coor = {ID: vid , Lat: lati , Lon: loni, Heading: headi, Distance: distance }
           Tstatus.push(coor);
+
+        //conditional logic
+          if(Tstatus[i].Distance <= 100){
+            alert('Train Approaching' + Tstatus[i].ID)
+            document.getElementById("stat").innerHTML("Train Approaching");
+            document.getElementById("notif").innerHTML(Tstatus[i]);
           }
-          Tlength = await Tstatus.length;
+
+
+          }
 
           //products of getTrain
           console.log(Tstatus);
-          console.log(Tlength);
 }
 
-getTrain();
-//GetUserLocation
-function getuloc(){
-if ('geolocation' in navigator) {
-  console.log('Geolocation available');
-  navigator.geolocation.getCurrentPosition(function(position) {
-    var ulat = position.coords.latitude;
-    var ulon = position.coords.longitude;
-    var ucoor = {lat: ulat, lon: ulon};
-    console.log(ucoor);
-  });
-} else {
-  console.log('geolocation not available');
-}
-}
 
-getuloc();
 
-}
-
-main();
-//Calculate Distance Between User and Trains
-async function getDist() {
-
-      for(k=0; k<Tlength; k++){
-      d = Math.sqrt(Math.pow(ulat-Tstatus[k].lat, 2) + Math.cos(Tstatus[k].lat)* Math.pow(ulon-Tstatus[k].lon, 2));
-      Tdist.push(d);
-    }
-    console.log(Tdist);
-}
-
-getDist();
+getuloc(getTrain);
 
 
 
